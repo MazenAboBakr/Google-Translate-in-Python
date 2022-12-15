a3218c671693e073347f19404ef120610956c0d3
MazenAboBakr <115697359+MazenAboBakr@users.noreply.github.com>
2022-12-15 22:32:51 +0200

    Update Translator.py

--- Translator.py
+++ Translator.py
@@ -1,6 +1,10 @@
+# Made By Mazen AboBakr 12/15/2022
+# Google Translate in Python 
+
 from googletrans import Translator, constants
 from pprint import pprint
 translator = Translator()
 
+# Translate to French
 translation = translator.translate("Hello World", dest="fr")
 print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
